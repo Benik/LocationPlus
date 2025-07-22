b8fd42cfca42b56a21be2fc0d34c06a9d90d1839
thurin <thurin@gmail.com>
2025-07-21 22:55:15 -0600

    LibTouristClassic is now release status for mists

--- ElvUI_LocPlus/libs/LibTouristClassic-1.0/LibTouristClassic-1.0.lua
+++ ElvUI_LocPlus/libs/LibTouristClassic-1.0/LibTouristClassic-1.0.lua
@@ -1,6 +1,6 @@
 --[[
 Name: LibTouristClassic-1.0
-Revision: $Rev: 268 $
+Revision: $Rev: 269 $
 Author(s): Odica; based on LibTourist-3.0
 Documentation: https://www.wowace.com/projects/libtourist-1-0/pages/api-reference
 Git: https://repos.wowace.com/wow/libtourist-classic libtourist-classic
@@ -9,7 +9,7 @@ License: MIT
 ]]
 
 local MAJOR_VERSION = "LibTouristClassic-1.0"
-local MINOR_VERSION = 90000 + tonumber(("$Revision: 268 $"):match("(%d+)"))
+local MINOR_VERSION = 90000 + tonumber(("$Revision: 269 $"):match("(%d+)"))
 
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub") end
 local C_Map = C_Map
